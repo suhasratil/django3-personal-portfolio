5871300829d422b5805ebe82a22f1212ce203c70
Suhas Patil <suhas.patil@klarna.com>
2021-12-26 15:42:25 +0100

    About page

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
+    path('about/', views.about, name='about'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
